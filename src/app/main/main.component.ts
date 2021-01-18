df7e5d3ebc2f221f14b661b568b7929acf9974ba
Keyshawn Butts <kjbappsllc@gmail.com>
2021-01-18 00:18:34 -0500

    did a lot of good work

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LowDBService } from '../core/services';
-import { CategoryGroup } from '../core/models/group';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-main',
@@ -10,10 +10,35 @@ import { CategoryGroup } from '../core/models/group';
 })
 export class MainComponent implements OnInit {
 
-  constructor(
-    private router: Router,
-    private db: LowDBService
-  ) {}
+  public sideBarCollapsed: boolean;
+  public sideBarHidden: boolean;
 
-  ngOnInit(): void { }
+  constructor(
+    private db: LowDBService,
+    private breakpointObs: BreakpointObserver
+  ) { }
+
+  ngOnInit(): void {
+    this.checkViewportForSidebar();
+  }
+
+  checkViewportForSidebar() {
+    const defaultCollapsedQuery = '(max-width: 1439px) and (min-width: 992px)';
+    const defaultHiddenQuery = '(max-width: 991px)'
+    this.breakpointObs
+      .observe([defaultCollapsedQuery, defaultHiddenQuery])
+      .subscribe((state: BreakpointState) => {
+        console.log(state);
+        if (state.breakpoints[defaultCollapsedQuery]) {
+          this.sideBarCollapsed = true;
+          this.sideBarHidden = false;
+        } else if (state.breakpoints[defaultHiddenQuery]) {
+          this.sideBarHidden = true;
+          this.sideBarCollapsed = false;
+        } else {
+          this.sideBarCollapsed = false;
+          this.sideBarHidden = false;
+        }
+      })
+  }
 }
